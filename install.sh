7c04e8f9476dcec4326dc857df4c636b818a63f2
Luis Emidio <luisfuturist@gmail.com>
2025-02-14 13:08:04 -0300

    feat: configure global git user settings in install script

--- install.sh
+++ install.sh
@@ -8,3 +8,11 @@ chmod +x /usr/local/bin/*
 
 # reload bashrc
 source ~/.bashrc
+
+# Git
+
+su - luis -c '  
+git config --global user.name "Luis Emidio" &&  
+git config --global user.email "luisfuturist@gmail.com" &&  
+git config --global core.editor "code --wait"  
+'
